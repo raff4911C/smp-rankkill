54992dac92b70e472e1ce439bdadec850834d086
raff4911C <raffalcarre@gmail.com>
2026-08-30 12:59:59 +0200

    Create RankCommand.java

---
+++ src/main/java/fr/raff/smpkill/command/RankCommand.java
@@ -0,0 +1,8 @@
+package fr.raff.smpkill.command;
+
+import com.mojang.brigadier.CommandDispatcher;
+import net.minecraft.server.command.ServerCommandSource;
+
+public class RankCommand {
+    public static void register(CommandDispatcher<ServerCommandSource> dispatcher) {}
+}
