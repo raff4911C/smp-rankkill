af6f332a0a4bcdd53b220acb645dacf9710cd206
raff4911C <raffalcarre@gmail.com>
2026-08-30 12:52:02 +0200

    Create BossBarManager.java

---
+++ src/main/java/fr/raff/smpkill/data/BossBarManager.java
@@ -0,0 +1,9 @@
+package fr.raff.smpkill.data;
+
+import net.minecraft.server.network.ServerPlayerEntity;
+
+public class BossBarManager {
+    public static void init() {}
+    public static void addPlayer(ServerPlayerEntity player) {}
+    public static void removePlayer(ServerPlayerEntity player) {}
+}
